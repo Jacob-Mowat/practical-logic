8b3ec4bc4ee67729e10fdee4775d9be36e100f2c
Jacob-Mowat <jacobmowat@outlook.com>
2016-05-02 11:39:08 +0100

    inital commit

--- practical-logic/main.cpp
+++ practical-logic/main.cpp
@@ -7,9 +7,26 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int n = 0;
+    std::string f = "";
+    
+    if (argc == 1)
+    {
+        std::cout << "Usage: ./logic -n <number_of_vars> -f <formula>" << std::endl;
+    }
+    else if (argv[2] != NULL & argv[4] != NULL)
+    {
+        n = std:: stoi(argv[2]);
+        f = argv[4];
+    }
+    else
+    {
+        std::cout << "You must pass both a number of varibles and a formula like so; \n ./logic -n <number_of_vars> -f <formula>" << std::endl;
+    }
+    
     return 0;
 }
