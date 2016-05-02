a21337d36173cab28bf8769d463d4d7a67d71ada
Jacob-Mowat <jacobmowat@outlook.com>
2016-05-02 11:35:24 +0100

    Initial Commit

---
+++ practical-logic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practical-logic
+//
+//  Created by Jacob Mowat on 02/05/2016.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
